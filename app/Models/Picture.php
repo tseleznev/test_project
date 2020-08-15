8fca53c2a4375b0b5714d8e318c3c57749c97b2a
Timur Seleznev <t.seleznev@outlook.com>
2020-08-15 16:04:08 +0600

    release test

---
+++ app/Models/Picture.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Picture extends Model
+{
+    protected $fillable = ['name', 'part_1', 'part_2', 'part_3', 'part_4'];
+}
